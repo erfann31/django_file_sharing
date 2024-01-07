b2ac5ee687e74276cec14753d1e9c09ae20deadf
erfann31 <erfannasri2@gmail.com>
2024-01-08 02:42:12 +0330

    fix sth

--- file_manager/urls.py
+++ file_manager/urls.py
@@ -1,8 +1,10 @@
+from django.contrib import admin
 from django.urls import path
 
 from file import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('upload/', views.upload_file, name='upload_file'),
     path('upload/success/', views.upload_success, name='file_upload_success'),
